bd1f1e371f1191b8366768eea46fdf5092dfb2a8
isolis <isolis@solavcr.com>
2018-11-01 18:58:47 -0600

    fixed POST and GET

--- src/org/ce1103/gos/util/Client.java
+++ src/org/ce1103/gos/util/Client.java
@@ -1,43 +1,62 @@
 package org.ce1103.gos.util;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.ws.WebServiceProvider;
 
 public class Client {
+	
+	private void postList(String strList) throws IOException {
+		String url = "http://desktop-6f6k4bq:9080/Proyecto2_GameofSorts_WebService/rest/sort/postList";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-	public static String getSortedList(int sort, String strList) throws IOException, JAXBException {
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", "text/plain");
+		String urlParameters = strList;
+		
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+		
+		int responseCode = con.getResponseCode();
+		System.out.println("Response code: " + responseCode);
+	}
+
+	private String getSortedList(int sort) throws IOException, JAXBException {
 		URL url = null;
 		if (sort == 0) {
-			url = new URL("http://desktop-6f6k4bq:9080/Proyecto2_GameofSorts_WebService/rest/sort/selectionSort?value=" + strList);
+			url = new URL("http://desktop-6f6k4bq:9080/Proyecto2_GameofSorts_WebService/rest/sort/selectionSort");
 		} else if (sort == 1) {
-			url = new URL("http://desktop-6f6k4bq:9080/Proyecto2_GameofSorts_WebService/rest/sort/insertionSort?value=" + strList);
+			url = new URL("http://desktop-6f6k4bq:9080/Proyecto2_GameofSorts_WebService/rest/sort/insertionSort");
 		} else if (sort == 2) {
-			url = new URL("http://desktop-6f6k4bq:9080/Proyecto2_GameofSorts_WebService/rest/sort/quickSort?value=" + strList);
+			url = new URL("http://desktop-6f6k4bq:9080/Proyecto2_GameofSorts_WebService/rest/sort/quickSort");
 		}
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		System.out.println("1");
 		con.setRequestMethod("GET");
 		int status = con.getResponseCode();
-		System.out.println("2");
 		InputStreamReader reader = new InputStreamReader(con.getInputStream());
 		char[] buffer = new char[1024];
 		int leidos = 0;
 		StringBuilder builder = new StringBuilder();
-		System.out.println("3");
 		while ((leidos = reader.read(buffer)) > 0) {
 			builder.append(new String(buffer, 0, leidos));
 //			String leido = new String(buffer, 0, leidos);
 			System.out.println(status);
 		}
-		System.out.println("4");
-		System.out.println(builder.toString());
 		JAXBContext jaxbContext = JAXBContext.newInstance(SortedList.class);
 	    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
@@ -47,8 +66,9 @@ public class Client {
 		return sortedList.listToSort;
 	}
 	
-	public static void main(String[] args) throws IOException, JAXBException {
-		System.out.println(Client.getSortedList(0, "[1,2,14,3]"));
+	public static String clientSort(int sort, String strList) throws IOException, JAXBException { //sorts (0=SelectionSort, 1=InsertionSort, 2=QuickSort)
+		Client client = new Client();
+		client.postList(strList);
+		return client.getSortedList(sort);
 	}
-
 }
